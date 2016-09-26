37d580972e5ab33935b7191654d555d9320270ee
Mathias Bellerby <Mathis2501@gmail.com>
2016-09-26 13:09:29 +0200

    Everything Passes

--- 09_Portfolio/Portfolio.cs
+++ 09_Portfolio/Portfolio.cs
@@ -8,5 +8,31 @@ namespace _09_Portfolio
 {
     class Portfolio
     {
+        public List<Asset> Stocks = new List<Asset>();
+        public Portfolio(List<Asset> stocks)
+        {
+            Stocks = stocks;
+        }
+
+        public Portfolio()
+        {
+
+        }
+
+        public double GetTotalValue()
+        {
+            double Total = 0;
+            foreach (Asset item in Stocks)
+            {
+                Total += item.GetValue();
+            }
+            return Total;
+        }
+
+        public void AddAsset(Asset OrigAsset)
+        {
+            Stocks.Add(OrigAsset);
+        }
+
     }
 }
