0f8afe57ddc8d39259294cef74cb3db7fb8c3086
Mathias Bellerby <Mathis2501@gmail.com>
2016-09-26 12:23:23 +0200

    Stuff Passes

---
+++ 09_Portfolio/Stock.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09_Portfolio
+{
+    class Stock : Asset
+    {
+
+        public string Symbol;
+        public double PricePerShare;
+        public double NumShares;
+
+        public Stock()
+        {
+            
+        }
+
+        public Stock(string symbol, double pricePerShare, double numShares)
+        {
+            Symbol = symbol;
+            PricePerShare = pricePerShare;
+            NumShares = numShares;
+        }
+
+        public double GetValue()
+        {
+            return NumShares * PricePerShare;
+        }
+
+        public static double TotalValue(Stock[] stocks)
+        {
+            double Total = 0;
+            foreach (Stock stock in stocks)
+            {
+                Total += stock.NumShares * stock.PricePerShare;
+            }
+            return Total;
+        }
+
+        public override string ToString()
+        {
+            return "Stock[symbol=" + Symbol + ",pricePerShare=" + PricePerShare.ToString().Replace(",",".") + ",numShares=" + NumShares + "]";
+        }
+
+        public bool Equals(Stock stock)
+        {
+            if (stock.Symbol == this.Symbol && stock.PricePerShare == this.PricePerShare && stock.NumShares == this.NumShares)
+                return true;
+            else
+                return false;
+        }
+
+
+
+    }
+}
